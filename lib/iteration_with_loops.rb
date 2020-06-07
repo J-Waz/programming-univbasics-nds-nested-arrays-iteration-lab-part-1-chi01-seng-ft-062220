366f95a07fc6ab28bfc51645a02d0a3778b1e118
Jacob G. Wazydrag <jgw.jakegeorge@gmail.com>
2020-06-07 09:52:17 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,13 @@
 def find_even_values(src)
 
-array_1 = [[1,2,3], [4,5,6], [7,8,9]]
+row_index = 0 
+while row_index < src.count do
+  element_index = 0 
+  while element_index < 
+  src[row_index].count.do 
+    element_index += 1 
+  end 
+  row_index += 1 
+end
 
 end
